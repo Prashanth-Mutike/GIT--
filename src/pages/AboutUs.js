2263e92f87cf87f84ce505a4c7b0bff197a62232
Prashanth-Mutike <prashanthmutike@gmail.com>
2023-08-19 15:04:17 +0530

    practicing git

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const AboutUs = () => {
+return (
+	<div className="home">
+	<h1>12345 About us</h1>
+	</div>
+);
+};
+
+export const OurAim = () => {
+return (
+	<div className="home">
+	<h1>TREW Aim</h1>
+	</div>
+);
+};
+
+export const OurVision = () => {
+return (
+	<div className="home">
+	<h1>ABCD Vision</h1>
+	</div>
+);
+};
